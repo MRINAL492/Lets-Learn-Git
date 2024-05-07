b203115ec2c48818d6d99d5f27c193e05cbc51ae
Mrinal Kumar Mishra <mrinalmishra302@gmail.com>
2024-05-07 17:52:01 +0530

    multiply.java committed in mrinal/multiply branch

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multply{
+    public static void main(String args[]){
+      int a = 5;
+      int b = 6;
+    
+      System.out.println("The product of a and b is "+(a*b));
+    }
+    }
